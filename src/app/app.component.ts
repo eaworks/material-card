3b4b9f4c26253907127814006189460c70d196ea
Ersin Aydin <ersinaydinworks@gmail.com>
2022-12-27 00:58:07 +0300

    datan and card module add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'material-card';
+  posts = [
+    {
+      title: 'Camping',
+      imageUrl: '/assets/camping.jpeg',
+      username: 'eaydin',
+      content: 'Kampa Gencler',
+    },
+    {
+      title: 'Tirmanis',
+      imageUrl: '/assets/paragliding.jpeg',
+      username: 'eaydin',
+      content: 'Bugunde iyi tirmandim',
+    },
+    {
+      title: 'Surf',
+      imageUrl: '/assets/surf.jpeg',
+      username: 'eaydin',
+      content: 'Hadi biraz surf yapalim',
+    },
+  ];
 }
