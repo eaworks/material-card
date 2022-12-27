5f590d8b5c09c5a6b0545a6bcb5aaa3a2723dd11
Ersin Aydin <ersinaydinworks@gmail.com>
2022-12-27 14:22:59 +0300

    images change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,19 @@ export class AppComponent {
   posts = [
     {
       title: 'Camping',
-      imageUrl: '/assets/camping.jpeg',
+      imageUrl: '/assets/camping.jpg',
       username: 'eaydin',
       content: 'Kampa Gencler',
     },
     {
       title: 'Tirmanis',
-      imageUrl: '/assets/paragliding.jpeg',
+      imageUrl: '/assets/paragliding.jpg',
       username: 'eaydin',
       content: 'Bugunde iyi tirmandim',
     },
     {
       title: 'Surf',
-      imageUrl: '/assets/surf.jpeg',
+      imageUrl: '/assets/surf.jpg',
       username: 'eaydin',
       content: 'Hadi biraz surf yapalim',
     },
